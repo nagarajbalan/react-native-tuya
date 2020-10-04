afe1787db3059802f71a8f8783c46cc5bb8ae727
Chamika Hasanthi <chamika.hasanthi@axiatadigitallabs.com>
2020-10-04 23:43:03 +0530

    Fix Syntax error in camera.ts

--- src/camera.ts
+++ src/camera.ts
@@ -2,14 +2,13 @@ import { NativeModules } from 'react-native';
 
 const tuya = NativeModules.TuyaCameraModule;
 
-export type CameraLivePreviewParams = { 
+export type CameraLivePreviewParams = {
   countryCode: string;
   uid: string;
   passwd: string;
   devId: string;
+};
 
-   };
-
-export function openCameraLivePreview(params: CameraLivePreviewParams) Promise<string> {
+export function openCameraLivePreview(params: CameraLivePreviewParams): Promise<string> {
   return tuya.openLivePreview(params);
 }
